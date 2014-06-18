b6f220cc934c53daa04169282e55aa5234a109be
Taizo Sadhiro <sadahiro@taizox201.(none)>
2014-06-18 13:58:24 +0900

    AllImp has been changed slightly

--- AllImpLozenge.java
+++ AllImpLozenge.java
@@ -17,19 +17,13 @@ class AllImpLozenge{
 	}
 
 	System.out.println(tiling.toString());
-	//System.out.println(tiling.impurities);
-	///System.out.println(tiling.isValid());
-	if (!tiling.isValid()) ;
 	HashMap<Coord,HashSet<Move>> possiblemoves = tiling.getMovable();
-	//System.out.println(possiblemoves);
 	for (Map.Entry<Coord,HashSet<Move>> moves: possiblemoves.entrySet()){
 	    Coord c = moves.getKey();
 	    
 	    for (Iterator iter = moves.getValue().iterator(); iter.hasNext();){
 		Move m = (Move)(iter.next());
 		Move reverse = new Move(m.type, tiling.dir.get(c));
-		//System.out.print(tiling.dir);
-		//System.out.println(" --> move: " + m + " reverse:" + reverse + " at " + c);
 		tiling.move(c,m);
 		search();
 		tiling.move(c,reverse);
@@ -39,7 +33,7 @@ class AllImpLozenge{
 
     public static void main(String[] args){
 
-	AllImpLozenge all = new AllImpLozenge(3);
+	AllImpLozenge all = new AllImpLozenge(5);
 	all.search();
 
     }
